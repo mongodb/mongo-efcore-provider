6e3eaa3c5369f43efa727bd7afe1657b8c66683f
Oleksandr Poliakov <oleksandr.poliakov@mongodb.com>
2023-09-06 11:42:41 -0700

    PR

---
+++ tests/MongoDB.EntityFrameworkCore.FunctionalTests/Query/CollectionsResponseTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MongoDB.EntityFrameworkCore.FunctionalTests.Entities.Guides;
+using XUnitCollection = Xunit.CollectionAttribute;
+
+namespace MongoDB.EntityFrameworkCore.FunctionalTests.Query;
+
+[XUnitCollection(nameof(SampleGuidesFixture))]
+public class CollectionsResponseTests
+{
+    private readonly GuidesDbContext _db;
+
+    public CollectionsResponseTests(SampleGuidesFixture fixture)
+    {
+        _db = GuidesDbContext.Create(fixture.MongoDatabase);
+    }
+
+    [Fact]
+    public void ToList()
+    {
+        var result = _db.Planets.ToList();
+        Assert.Equal(8, result.Count);
+    }
+
+    [Fact]
+    public async Task ToListAsync()
+    {
+        var result = await _db.Planets.ToListAsync();
+        Assert.Equal(8, result.Count);
+    }
+
+    [Fact]
+    public void ToArray()
+    {
+        var result = _db.Planets.ToArray();
+        Assert.Equal(8, result.Length);
+    }
+
+    [Fact]
+    public async Task ToArrayAsync()
+    {
+        var result = await _db.Planets.ToArrayAsync();
+        Assert.Equal(8, result.Length);
+    }
+}
