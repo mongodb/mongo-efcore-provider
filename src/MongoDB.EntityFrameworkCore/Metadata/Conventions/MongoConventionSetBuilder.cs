ac9d96583841bdf3bdcf9b1aad35fd0f52d4fb2f
Damien Guard <damien.guard@mongodb.com>
2023-09-26 21:06:11 +0100

    Small clean-up. (#24)

--- src/MongoDB.EntityFrameworkCore/Metadata/Conventions/MongoConventionSetBuilder.cs
+++ src/MongoDB.EntityFrameworkCore/Metadata/Conventions/MongoConventionSetBuilder.cs
@@ -1,17 +1,17 @@
 ﻿/* Copyright 2023-present MongoDB Inc.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
@@ -46,7 +46,7 @@ public class MongoConventionSetBuilder : ProviderConventionSetBuilder
         // New MongoDB-specific conventions
         conventionSet.Add(new CollectionNameFromDbSetConvention(Dependencies));
         conventionSet.Add(new CollectionAttributeConvention(Dependencies));
-        conventionSet.Add(new PrimaryKeyDiscoveryConvention(Dependencies));
+        conventionSet.Replace<KeyDiscoveryConvention>(new PrimaryKeyDiscoveryConvention(Dependencies));
 
         // Convenience conventions for users familiar with EF
         conventionSet.Add(new TableAttributeConvention(Dependencies));
