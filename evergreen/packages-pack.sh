9b9ce36297d74b183a0b886c806842a9c6a6e6f6
Oleksandr Poliakov <oleksandr.poliakov@mongodb.com>
2023-10-11 15:18:12 -0700

    Package script

---
+++ evergreen/packages-pack.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -o errexit  # Exit the script with error if any of the commands fail
+
+if [ -z "$PACKAGE_VERSION" ]; then
+  PACKAGE_VERSION=$(sh ./evergreen/packages-version.sh)
+  echo Calculated PACKAGE_VERSION value: "$PACKAGE_VERSION"
+fi
+
+echo Creating nuget package...
+
+dotnet clean ./MongoDB.EFCoreProvider.sln
+dotnet pack ./MongoDB.EFCoreProvider.sln -o ./build/nuget -c Release -p:Version="$PACKAGE_VERSION" --include-symbols -p:SymbolPackageFormat=snupkg
