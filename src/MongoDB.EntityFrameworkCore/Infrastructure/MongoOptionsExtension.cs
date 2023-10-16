562448fe648497a5dfbd2d98101015a263526247
Damien Guard <damien.guard@mongodb.com>
2023-10-16 20:12:17 +0100

    Support .NET 6. (#34)

--- src/MongoDB.EntityFrameworkCore/Infrastructure/MongoOptionsExtension.cs
+++ src/MongoDB.EntityFrameworkCore/Infrastructure/MongoOptionsExtension.cs
@@ -1,17 +1,17 @@
 ﻿/* Copyright 2023-present MongoDB Inc.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 using System;
 using System.Collections.Generic;
@@ -20,6 +20,7 @@ using System.Text;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using MongoDB.Driver;
+using MongoDB.EntityFrameworkCore;
 
 // ReSharper disable once CheckNamespace (extensions should be in the EF namespace for discovery)
 namespace Microsoft.EntityFrameworkCore;
@@ -98,7 +99,7 @@ public class MongoOptionsExtension : IDbContextOptionsExtension
     /// <returns>The <see cref="MongoOptionsExtension"/> to continue chaining configuration.</returns>
     public virtual MongoOptionsExtension WithDatabaseName(string databaseName)
     {
-        ArgumentException.ThrowIfNullOrEmpty(databaseName);
+        databaseName.ThrowArgumentExceptionIfNullOrEmpty();
 
         var clone = Clone();
         clone._databaseName = databaseName;
