b0b7445f82651da6666662e963778e23ceedeb5c
Damien Guard <damien.guard@mongodb.com>
2024-04-26 23:02:45 +0100

    Throw helpful error message if transactions attempted. (#73)

---
+++ src/MongoDB.EntityFrameworkCore/Storage/MongoTransactionManager.cs
@@ -0,0 +1,77 @@
+﻿/* Copyright 2023-present MongoDB Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Transactions;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace MongoDB.EntityFrameworkCore.Storage;
+
+/// <summary>
+/// Placeholder for a MongoDB transaction manager that for now just flags that transactions
+/// are not supported in case somebody attempts to use them.
+/// </summary>
+public class MongoTransactionManager : IDbContextTransactionManager, ITransactionEnlistmentManager
+{
+    /// <inheritdoc />
+    public void ResetState()
+    {
+    }
+
+    /// <inheritdoc />
+    public Task ResetStateAsync(CancellationToken cancellationToken = new())
+        => Task.CompletedTask;
+
+    /// <inheritdoc />
+    public IDbContextTransaction BeginTransaction()
+        => throw CreateNotSupportedException();
+
+    /// <inheritdoc />
+    public Task<IDbContextTransaction> BeginTransactionAsync(CancellationToken cancellationToken = new())
+        => throw CreateNotSupportedException();
+
+    /// <inheritdoc />
+    public void CommitTransaction()
+        => throw CreateNotSupportedException();
+
+    /// <inheritdoc />
+    public Task CommitTransactionAsync(CancellationToken cancellationToken = new())
+        => throw CreateNotSupportedException();
+
+    /// <inheritdoc />
+    public void RollbackTransaction()
+        => throw CreateNotSupportedException();
+
+    /// <inheritdoc />
+    public Task RollbackTransactionAsync(CancellationToken cancellationToken = new())
+        => throw CreateNotSupportedException();
+
+    /// <inheritdoc />
+    public void EnlistTransaction(Transaction? transaction)
+        => throw CreateNotSupportedException();
+
+    /// <inheritdoc />
+    public Transaction? EnlistedTransaction
+        => null;
+
+    /// <inheritdoc />
+    public IDbContextTransaction? CurrentTransaction
+        => null;
+
+    private static NotSupportedException CreateNotSupportedException()
+        => new("The MongoDB EF Core Provider does not support transactions.");
+}
---
+++ tests/MongoDB.EntityFrameworkCore.UnitTests/Storage/MongoTransactionManagerTests.cs
@@ -0,0 +1,47 @@
+﻿/* Copyright 2023-present MongoDB Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using MongoDB.EntityFrameworkCore.Storage;
+
+namespace MongoDB.EntityFrameworkCore.UnitTests.Storage;
+
+public class MongoTransactionManagerTests
+{
+    [Fact]
+    public async Task Does_not_support_transactions()
+    {
+        var transactionManager = new MongoTransactionManager();
+
+        Assert.Throws<NotSupportedException>(() => transactionManager.BeginTransaction());
+        await Assert.ThrowsAsync<NotSupportedException>(async () => await transactionManager.BeginTransactionAsync());
+
+        Assert.Throws<NotSupportedException>(() => transactionManager.CommitTransaction());
+        await Assert.ThrowsAsync<NotSupportedException>(async () => await transactionManager.CommitTransactionAsync());
+
+        Assert.Throws<NotSupportedException>(() => transactionManager.RollbackTransaction());
+        await Assert.ThrowsAsync<NotSupportedException>(async () => await transactionManager.RollbackTransactionAsync());
+
+        Assert.Null(transactionManager.CurrentTransaction);
+        Assert.Null(transactionManager.EnlistedTransaction);
+
+        Assert.Throws<NotSupportedException>(() => transactionManager.EnlistTransaction(null));
+
+        transactionManager.ResetState();
+        await transactionManager.ResetStateAsync();
+
+        Assert.Null(transactionManager.CurrentTransaction);
+        Assert.Null(transactionManager.EnlistedTransaction);
+    }
+}
