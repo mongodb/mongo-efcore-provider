66f3424f5c91ff52f73a0efc1a2f86ed3187b970
Damien Guard <damieng@gmail.com>
2024-04-29 19:33:50 +0100

    Provide better error messaging fon untranslatable LINQ.

---
+++ tests/MongoDB.EntityFrameworkCore.FunctionalTests/Query/InvalidQueryTranslationTests.cs
@@ -0,0 +1,55 @@
+﻿/* Copyright 2023-present MongoDB Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace MongoDB.EntityFrameworkCore.FunctionalTests.Query;
+
+public class InvalidQueryTranslationTests : IClassFixture<TemporaryDatabaseFixture>
+{
+    private readonly TemporaryDatabaseFixture _tempDatabase;
+
+    public InvalidQueryTranslationTests(TemporaryDatabaseFixture tempDatabase)
+    {
+        _tempDatabase = tempDatabase;
+    }
+
+    class SimpleEntity
+    {
+        public Guid _id { get; set; }
+        public string name { get; set; }
+    }
+
+    [Fact]
+    public void ExecuteDelete_throws_invalid_operation_exception()
+    {
+        var db = SingleEntityDbContext.Create(_tempDatabase.CreateTemporaryCollection<SimpleEntity>());
+
+        var ex = Assert.Throws<InvalidOperationException>(() => db.Entities.ExecuteDelete());
+        Assert.Contains("ExecuteDelete", ex.Message);
+        Assert.Contains("LINQ expression", ex.Message);
+    }
+}
+
+static class FakeQueryableExtensions
+{
+    internal static int ExecuteDelete<TSource>(this IQueryable<TSource> source)
+        => source.Provider.Execute<int>(Expression.Call(ExecuteDeleteMethodInfo.MakeGenericMethod(typeof(TSource)),
+            source.Expression));
+
+    private static readonly MethodInfo ExecuteDeleteMethodInfo
+        = typeof(FakeQueryableExtensions).GetTypeInfo().GetDeclaredMethod(nameof(ExecuteDelete))!;
+}
