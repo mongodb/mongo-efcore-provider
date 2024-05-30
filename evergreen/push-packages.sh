a2ae6271621fe93a6d4831867f839d957dcedb5f
Oleksandr Poliakov <oleksandr.poliakov@mongodb.com>
2024-05-30 16:51:00 -0700

    EF-119: Onboard EF to Papertrail Service for SSDLC Compliance

--- evergreen/push-packages.sh
+++ evergreen/push-packages.sh
@@ -2,6 +2,32 @@
 set -o errexit  # Exit the script with error if any of the commands fail
 set +o xtrace # Disable tracing.
 
+# querying nuget source to find search base url
+packages_search_url=$(curl -X GET -s "${PACKAGES_SOURCE}" | jq -r 'first(.resources[] | select(."@type"=="SearchQueryService") | ."@id")')
+
+wait_until_package_is_available ()
+{
+  package=$1
+  version=$2
+  resp=""
+  count=0
+  echo "Checking package availability: ${package}:${version} at ${packages_search_url}"
+  while [ -z "$resp" ] && [ $count -le 40 ]; do
+    resp=$(curl -X GET -s "$packages_search_url?prerelease=true&take=1&q=PackageId:$package" | jq --arg jq_version "$version" '.data[0].versions[] | select(.version==$jq_version) | .version')
+    if [ -z "$resp" ]; then
+      echo "sleeping for 15 seconds..."
+      sleep 15
+    fi
+  done
+
+  if [ -z "$resp" ]; then
+    echo "Timeout while waiting for package availability: ${package}"
+    exit 1
+  else
+    echo "Package ${package} is available, version: ${resp}"
+  fi
+}
+
 if [ -z "$PACKAGES_SOURCE" ]; then
   echo "PACKAGES_SOURCE variable should be set"
   exit 1
@@ -18,3 +44,5 @@ if [ -z "$PACKAGE_VERSION" ]; then
 fi
 
 dotnet nuget push --source "$PACKAGES_SOURCE" --api-key "$PACKAGES_SOURCE_KEY" ./artifacts/nuget/MongoDB.EntityFrameworkCore."$PACKAGE_VERSION".nupkg
+
+wait_until_package_is_available "MongoDB.EntityFrameworkCore" "$PACKAGE_VERSION"
