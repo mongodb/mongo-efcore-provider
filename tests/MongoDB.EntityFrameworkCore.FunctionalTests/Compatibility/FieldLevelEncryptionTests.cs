5b1986b61f5d17459e19118dcc46e9c57b807b03
Damien Guard <damien.guard@mongodb.com>
2024-12-28 10:48:27 +0000

    Add client side encryption tests (#171)
    * Add client side encryption tests.
    * Make test report missing env vars.
    * Bring in CRYPT_SHARED_LIB_PATH to CI.

---
+++ tests/MongoDB.EntityFrameworkCore.FunctionalTests/Compatibility/FieldLevelEncryptionTests.cs
@@ -0,0 +1,198 @@
+﻿/* Copyright 2023-present MongoDB Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Security.Cryptography;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Encryption;
+
+namespace MongoDB.EntityFrameworkCore.FunctionalTests.Compatibility;
+
+[XUnitCollection("FieldLevelEncryption")]
+public class FieldLevelEncryptionTests(TemporaryDatabaseFixture database)
+    : IClassFixture<TemporaryDatabaseFixture>
+{
+    private readonly Dictionary<string, IReadOnlyDictionary<string, object>> _kmsProviders =
+        CreateKmsProvidersWithLocalMasterKey(CreateMasterKey());
+
+    private readonly CollectionNamespace _keyVaultNamespace =
+        CollectionNamespace.FromFullName(database.MongoDatabase.DatabaseNamespace.DatabaseName + "._keyVault");
+
+    static FieldLevelEncryptionTests()
+    {
+        MongoClientSettings.Extensions.AddAutoEncryption();
+    }
+
+    [Theory]
+    [InlineData(CryptProvider.Mongocryptd)]
+    [InlineData(CryptProvider.AutoEncryptSharedLibrary)]
+    public void Encrypted_data_can_round_trip(CryptProvider cryptProvider)
+    {
+        // Create new key for this collection
+        var patientDataKey = CreateDataKey(database.Client, _keyVaultNamespace, _kmsProviders);
+        var collectionName = "patients_" + cryptProvider;
+
+        // Create the schema map and an encrypted client
+        var schemaMap = CreateSchemaMap(collectionName, patientDataKey);
+        var encryptedClient = CreateEncryptedClient(_keyVaultNamespace, _kmsProviders, cryptProvider, schemaMap);
+
+        // Insert test data using the driver
+        var collection = encryptedClient.GetDatabase(database.MongoDatabase.DatabaseNamespace.DatabaseName)
+            .GetCollection<Patient>(collectionName);
+        collection.InsertMany([
+            new Patient
+            {
+                name = "Jon Doe",
+                ssn = "145014000",
+                bloodType = "AB-",
+                medicalRecords = [new MedicalRecord {weight = 180, bloodPressure = "120/80"}]
+            },
+            new Patient {name = "Tom Smith", ssn = "1234567", bloodType = "O-", medicalRecords = []}
+        ]);
+
+        // Test driver load
+        Assert.Equal("145014000", collection.AsQueryable().First().ssn);
+
+        // Test EF Core load & save
+        {
+            using var db = SingleEntityDbContext.Create(collection);
+            var patient = db.Entities.First();
+            Assert.Equal("145014000", patient.ssn);
+            patient.bloodType = "O-";
+            db.SaveChanges();
+        }
+
+        // Ensure saved data is correct via Driver & EF
+        Assert.Equal("O-", collection.AsQueryable().First().bloodType);
+        Assert.Equal("O-", SingleEntityDbContext.Create(collection).Entities.First().bloodType);
+
+        // EF Query
+        {
+            using var db = SingleEntityDbContext.Create(collection);
+            var patient = db.Entities.First(p => p.ssn == "145014000");
+            Assert.Equal("145014000", patient.ssn);
+        }
+    }
+    private Dictionary<string, BsonDocument> CreateSchemaMap(string collectionName, Guid patientDataKey)
+        => new()
+        {
+            {
+                database.MongoDatabase.DatabaseNamespace.DatabaseName + "." + collectionName,
+                CreatePatientEncryptionSchema(patientDataKey)
+            }
+        };
+
+    private static byte[] CreateMasterKey()
+        => RandomNumberGenerator.GetBytes(96);
+
+    private static Guid CreateDataKey(
+        IMongoClient client,
+        CollectionNamespace keyVaultNamespace,
+        Dictionary<string, IReadOnlyDictionary<string, object>> kmsProviders)
+    {
+        var clientEncryptionOptions = new ClientEncryptionOptions(client, keyVaultNamespace, kmsProviders);
+        using var clientEncryption = new ClientEncryption(clientEncryptionOptions);
+        return clientEncryption.CreateDataKey("local", new DataKeyOptions(), CancellationToken.None);
+    }
+
+    private static Dictionary<string, IReadOnlyDictionary<string, object>> CreateKmsProvidersWithLocalMasterKey(byte[] masterKey)
+        => new() {{"local", new Dictionary<string, object> {{"key", masterKey}}}};
+
+    private const string AesDeterministic = "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic";
+    private const string AesRandom = "AEAD_AES_256_CBC_HMAC_SHA_512-Random";
+
+    private static BsonDocument CreatePatientEncryptionSchema(Guid dataKey)
+        => new()
+        {
+            {"bsonType", "object"},
+            {
+                "encryptMetadata",
+                new BsonDocument
+                {
+                    {"algorithm", AesDeterministic},
+                    {"keyId", new BsonArray([new BsonBinaryData(dataKey, GuidRepresentation.Standard)])}
+                }
+            },
+            {
+                "properties",
+                new BsonDocument
+                {
+                    {"ssn", new BsonDocument {{"encrypt", new BsonDocument {{"bsonType", "string"}}}}},
+                    {
+                        "bloodType",
+                        new BsonDocument {{"encrypt", new BsonDocument {{"bsonType", "string"}, {"algorithm", AesRandom}}}}
+                    },
+                    {
+                        "medicalRecords",
+                        new BsonDocument {{"encrypt", new BsonDocument {{"bsonType", "array"}, {"algorithm", AesRandom}}}}
+                    }
+                }
+            }
+        };
+
+    public enum CryptProvider
+    {
+        AutoEncryptSharedLibrary,
+        Mongocryptd
+    };
+
+    private MongoClient CreateEncryptedClient(
+        CollectionNamespace keyVaultNamespace,
+        Dictionary<string, IReadOnlyDictionary<string, object>> kmsProviders,
+        CryptProvider cryptProvider,
+        Dictionary<string, BsonDocument> schemaMap)
+    {
+        var extraOptions = cryptProvider == CryptProvider.Mongocryptd
+            ? GetExtraOptionsForMongocryptd()
+            : GetExtraOptionsForCryptShared();
+
+        var clientSettings = database.Client.Settings.Clone();
+        clientSettings.AutoEncryptionOptions = new AutoEncryptionOptions(
+            keyVaultNamespace,
+            kmsProviders,
+            schemaMap: schemaMap,
+            extraOptions: extraOptions);
+        return new MongoClient(clientSettings);
+    }
+
+    private static Dictionary<string, object> GetExtraOptionsForCryptShared()
+        => new()
+        {
+            {"cryptSharedLibPath", GetEnvironmentVariableOrThrow("CRYPT_SHARED_LIB_PATH")},
+            {"cryptSharedLibRequired", true}
+        };
+
+    private static Dictionary<string, object> GetExtraOptionsForMongocryptd()
+        => new() {{"mongocryptdSpawnPath", GetEnvironmentVariableOrThrow("MONGODB_BINARIES")}};
+
+    private static string GetEnvironmentVariableOrThrow(string variable)
+        => Environment.GetEnvironmentVariable(variable) ?? throw new Exception($"Environment variable \"{variable}\" not set.");
+
+    class Patient
+    {
+        public ObjectId _id { get; set; }
+        public string name { get; set; }
+        public string ssn { get; set; }
+        public string bloodType { get; set; }
+        public List<MedicalRecord> medicalRecords { get; set; }
+    }
+
+    class MedicalRecord
+    {
+        public DateTimeOffset when { get; set; }
+        public int weight { get; set; }
+        public string bloodPressure { get; set; }
+    }
+}
