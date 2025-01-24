1bc8a3f8a308ae1df662f603799103e914ffbc5b
Oleksandr Poliakov <oleksandr.poliakov@mongodb.com>
2025-01-23 19:17:04 -0800

    Add build scripts

--- evergreen/pack-packages.sh
+++ evergreen/pack-packages.sh
@@ -6,7 +6,12 @@ if [ -z "$PACKAGE_VERSION" ]; then
   echo Calculated PACKAGE_VERSION value: "$PACKAGE_VERSION"
 fi
 
-echo Creating nuget package...
+BUILD_CONFIGURATION="Release"
+if [[ "${PACKAGE_VERSION}" == "9."* ]]; then
+    BUILD_CONFIGURATION="Release EF9"
+fi
+
+echo Creating nuget package $PACKAGE_VERSION using $BUILD_CONFIGURATION build configuration...
 
 dotnet clean ./MongoDB.EFCoreProvider.sln
-dotnet pack ./MongoDB.EFCoreProvider.sln -o ./artifacts/nuget -c Release -p:Version="$PACKAGE_VERSION" --include-symbols -p:SymbolPackageFormat=snupkg -p:ContinuousIntegrationBuild=true
+dotnet pack ./MongoDB.EFCoreProvider.sln -o ./artifacts/nuget -c "$BUILD_CONFIGURATION" -p:Version="$PACKAGE_VERSION" --include-symbols -p:SymbolPackageFormat=snupkg -p:ContinuousIntegrationBuild=true
--- evergreen/run-tests.sh
+++ evergreen/run-tests.sh
@@ -2,6 +2,7 @@
 set -o errexit  # Exit the script with error if any of the commands fail
 
 MONGODB_URI=${MONGODB_URI:=mongodb://localhost:27017/}
+BUILD_CONFIGURATION=${BUILD_CONFIGURATION:=Debug}
 
 EFCORE_PROVIDER_PROJECT_PATH="./src/MongoDB.EntityFrameworkCore/MongoDB.EntityFrameworkCore.csproj"
 if [ -n "$DRIVER_VERSION" ]
@@ -12,13 +13,13 @@ then
     echo "Installing the latest version of MongoDB.Driver..."
     dotnet remove "$EFCORE_PROVIDER_PROJECT_PATH" package MongoDB.Driver
     dotnet add "$EFCORE_PROVIDER_PROJECT_PATH" package MongoDB.Driver
-  elif [ -n "$DRIVER_VERSION" ]
-  then
+  else
     echo "Installing the $DRIVER_VERSION version of MongoDB.Driver..."
     dotnet remove "$EFCORE_PROVIDER_PROJECT_PATH" package MongoDB.Driver
     dotnet add "$EFCORE_PROVIDER_PROJECT_PATH" package MongoDB.Driver -v "$DRIVER_VERSION"
   fi
 fi
 
+echo "Running EF Core tests for '${BUILD_CONFIGURATION}' configuration"
 dotnet clean "./MongoDB.EFCoreProvider.sln"
-dotnet test "./MongoDB.EFCoreProvider.sln" -e MONGODB_URI="${MONGODB_URI}" --results-directory ./artifacts/test-results --logger "junit;LogFileName=TEST_{assembly}.xml;FailureBodyFormat=Verbose" --logger "console;verbosity=detailed"
+dotnet test "./MongoDB.EFCoreProvider.sln" -e MONGODB_URI="${MONGODB_URI}" -c "${BUILD_CONFIGURATION}" --results-directory ./artifacts/test-results/${BUILD_CONFIGURATION// /} --logger "junit;LogFileName=TEST_{assembly}.xml;FailureBodyFormat=Verbose" --logger "console;verbosity=detailed"
