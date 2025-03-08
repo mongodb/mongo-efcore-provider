ea22b6cf9643f407f710c056050d413014c20404
Maxwell Weru <mburumaxwell@gmail.com>
2025-03-08 14:48:54 +0000

    Support setting BSON representation on PropertiesConfigurationBuilder
    This allows configuration of properties of a certain type to have one particular representation in the whole context as compared to having this done from one property to another.

---
+++ tests/MongoDB.EntityFrameworkCore.UnitTests/Extensions/MongoPropertiesConfigurationBuilderExtensionsTests.cs
@@ -0,0 +1,87 @@
+﻿/* Copyright 2023-present MongoDB Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using MongoDB.Bson;
+
+namespace MongoDB.EntityFrameworkCore.UnitTests.Extensions;
+
+public class MongoPropertiesConfigurationBuilderExtensionsTests
+{
+    [Theory]
+    [InlineData(BsonType.String)]
+    [InlineData(BsonType.Double)]
+    [InlineData(BsonType.Decimal128)]
+    [InlineData(BsonType.Int64)]
+    public void HaveBsonRepresentation_can_set_type_on_property(BsonType bsonType)
+    {
+        using var db = new TestDbContext(bsonType);
+
+        var property = db.GetProperty((TestEntity t) => t.DateTimeOffsetProperty);
+        Assert.NotNull(property);
+
+        var representation = property.GetBsonRepresentation();
+        Assert.NotNull(representation);
+    }
+
+    [Theory]
+    [InlineData(BsonType.String, false, true)]
+    [InlineData(BsonType.Double, false, false)]
+    [InlineData(BsonType.Decimal128, null, false)]
+    [InlineData(BsonType.Int64, true, true)]
+    public void HaveBsonRepresentation_can_set_type_overflow_and_truncation_on_property(BsonType bsonType, bool? allowOverflow, bool? allowTruncation)
+    {
+        using var db = new TestDbContext(bsonType, allowOverflow, allowTruncation);
+
+        var property = db.GetProperty((TestEntity t) => t.DateTimeOffsetProperty);
+        Assert.NotNull(property);
+
+        var representation = property.GetBsonRepresentation();
+        Assert.NotNull(representation);
+        Assert.Equal(bsonType, representation.BsonType);
+        Assert.Equal(allowOverflow, representation.AllowOverflow);
+        Assert.Equal(allowTruncation, representation.AllowTruncation);
+    }
+
+
+    private class TestEntity
+    {
+        public int Id { get; set; }
+
+        public DateTimeOffset DateTimeOffsetProperty { get; set; }
+
+        public DateTimeOffset? NullableDateTimeOffsetProperty { get; set; }
+
+        public string StringProperty { get; set; }
+    }
+
+    private class TestDbContext(BsonType bsonType, bool? allowOverflow = null, bool? allowTruncation = null) : DbContext
+    {
+        public DbSet<TestEntity> Tests { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseMongoDB("mongodb://localhost:27017", $"UnitTests{Guid.NewGuid()}")
+                .ConfigureWarnings(x => x.Ignore(CoreEventId.ManyServiceProvidersCreatedWarning));
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+
+            configurationBuilder.Properties<DateTimeOffset>().HaveBsonRepresentation(bsonType, allowOverflow, allowTruncation);
+        }
+    }
+}
