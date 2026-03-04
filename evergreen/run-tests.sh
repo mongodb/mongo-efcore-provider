a3cf65d545c64d14a4fb4988a7449bef17b3fc74
Oleksandr Poliakov <31327136+sanych-sun@users.noreply.github.com>
2026-03-04 12:09:05 -0800

    EF-302: Fix build script to proper resolution of the DRIVER_VERSION (#284)

--- evergreen/run-tests.sh
+++ evergreen/run-tests.sh
@@ -2,21 +2,9 @@
 set -o errexit  # Exit the script with error if any of the commands fail
 
 MONGODB_URI=${MONGODB_URI:=mongodb://localhost:27017/}
-EFCORE_PROVIDER_PROJECT_PATH="./src/MongoDB.EntityFrameworkCore/MongoDB.EntityFrameworkCore.csproj"
 
-if [ -n "$DRIVER_VERSION" ]
-then
-  ## Update Driver's package reference if specified
-  if [ "$DRIVER_VERSION" = "latest" ]
-  then
-    echo "Installing the latest version of MongoDB.Driver..."
-    dotnet remove "$EFCORE_PROVIDER_PROJECT_PATH" package MongoDB.Driver
-    dotnet add "$EFCORE_PROVIDER_PROJECT_PATH" package MongoDB.Driver
-  else
-    echo "Installing the $DRIVER_VERSION version of MongoDB.Driver..."
-    dotnet remove "$EFCORE_PROVIDER_PROJECT_PATH" package MongoDB.Driver
-    dotnet add "$EFCORE_PROVIDER_PROJECT_PATH" package MongoDB.Driver -v "$DRIVER_VERSION"
-  fi
+if [ "$DRIVER_VERSION" = "latest" ]; then
+  export DRIVER_VERSION="*"
 fi
 
 echo "Running EF Core tests for '${BUILD_CONFIGURATION}' configuration"
