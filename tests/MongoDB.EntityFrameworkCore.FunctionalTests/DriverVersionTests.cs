e0b468184baccff03e65568308f755f1f68f58f5
Oleksandr Poliakov <oleksandr.poliakov@mongodb.com>
2026-03-05 12:08:00 -0800

    EF-306: Adjust CI not to bump the driver's version to the latest

---
+++ tests/MongoDB.EntityFrameworkCore.FunctionalTests/DriverVersionTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using MongoDB.Driver;
+
+namespace MongoDB.EntityFrameworkCore.FunctionalTests;
+
+public class DriverVersionTests
+{
+    [Fact]
+    public void TestDriverVersion()
+    {
+        var envDriverVersion = Environment.GetEnvironmentVariable("DRIVER_VERSION");
+        if (string.IsNullOrEmpty(envDriverVersion) || envDriverVersion == "latest")
+        {
+            return;
+        }
+
+        var driverAssembly = typeof(IMongoClient).Assembly;
+        var versionAttribute = driverAssembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+        var driverVersion = versionAttribute.InformationalVersion;
+        var hashIndex = versionAttribute.InformationalVersion.IndexOf('+');
+        if (hashIndex != -1)
+        {
+            driverVersion = versionAttribute.InformationalVersion.Substring(0, hashIndex);
+        }
+
+        Assert.Equal(envDriverVersion, driverVersion);
+    }
+}
+
