f9d3a7e718a3d96906a55654895814e86a7581bd
Arthur Vickers <ajcvickers@hotmail.com>
2026-05-27 15:08:58 +0100

    Fix to support servers before and after the metadata break.

--- tests/MongoDB.EntityFrameworkCore.FunctionalTests/Storage/IndexTests.cs
+++ tests/MongoDB.EntityFrameworkCore.FunctionalTests/Storage/IndexTests.cs
@@ -701,7 +701,6 @@ public class IndexTests(AtlasTemporaryDatabaseFixture database)
 
         Assert.Equal("READY", index["status"].AsString);
         Assert.True(index["queryable"].AsBoolean);
-        Assert.Equal(0, index["latestDefinitionVersion"]["version"].AsInt32);
 
         var field = fields[0];
         Assert.Equal("vector", field["type"].AsString);
